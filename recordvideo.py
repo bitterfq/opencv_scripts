c891343da204cf6c1b4f5ed0677a3d5cc43f5c0e
amancern3 <amandeep842.as@gmail.com>
2020-06-19 19:06:22 -0700

    Added video format and write methods.

--- recordvideo.py
+++ recordvideo.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np 
 import cv2
 
@@ -28,17 +29,39 @@ def get_dims(cap, res='1080p'):
     change_res(cap, width, height)
     return width, height
 
+
+# Video Encoding, might require additional installs
+# Types of Codes: http://www.fourcc.org/codecs.php
+VIDEO_TYPE = {
+    #VideoWriter_fourcc -- contains videocodex for each system
+    'avi': cv2.VideoWriter_fourcc(*'XVID'),
+    #'mp4': cv2.VideoWriter_fourcc(*'H264'),
+    'mp4': cv2.VideoWriter_fourcc(*'XVID'),
+}
+
+
+def get_video_type(filename):
+    filename, ext = os.path.splitext(filename)
+    if ext in VIDEO_TYPE:
+      return VIDEO_TYPE[ext]
+    return VIDEO_TYPE['avi']
+
+
 ##ffmpeg - for audio file
 cap = cv2.VideoCapture(0) #Single capture device 
 dims = get_dims(cap, res=def_res)
 
+#writing  method -- here we get def video type
+video_type_cv2 = get_video_type(filename)
+# writes out video
+out = cv2.VideoWriter(filenamem, video_type_cv2, frames_per_second, dims)
 while True:  # Continous reading of video
     ret, frame = cap.read()
     
     #change video rgb to gray
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
     #cv2.imshow('gray', gray)
-
+    out.write(frame)
     cv2.imshow('frame', frame) #imgshow doesn't work
     cv2.waitKey(1) # linux constant video frame works with waitKey(1)
     if cv2.waitKey(20) & 0xFF == ord('q'):
